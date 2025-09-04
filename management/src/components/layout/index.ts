fa8dbfb992f2220d7a73f74b0e280211196b2ccd
Victor Oseghale <voseghale1@gmail.com>
2025-09-04 05:05:53 +0200

    Restored frontend files

--- management/src/components/layout/index.ts
+++
@@ -1,9 +0,0 @@
-// Layout components barrel export
-export { default as Layout } from './Layout';
-export { default as Header } from './Header';
-export { default as Sidebar } from './Sidebar';
-
-// Re-export types
-export type { LayoutProps } from './Layout';
-export type { HeaderProps } from './Header';
-export type { SidebarProps, SidebarItem } from './Sidebar';
